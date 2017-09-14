cc7fc44555d7c1f72320e18be086a898cc1d0085
tom <tomoinn@gmail.com>
2017-09-14 13:55:10 +0100

    Code reformatting otherwise PyCharm will keep trying to do it...

--- src/python/approxeng/input/pihut.py
+++ src/python/approxeng/input/pihut.py
@@ -3,6 +3,7 @@ from approxeng.input import CentredAxis, Controller, Button, TriggerAxis, Binary
 PH_VENDOR_ID = 9571
 PH_PRODUCT_ID = 1318
 
+
 class PiHut(Controller):
     """
     Driver for the PiHut PS3-alike controller
@@ -18,32 +19,32 @@ class PiHut(Controller):
             Used to set the hot zone for each :class:`approxeng.input.CentredAxis` in the controller.
         """
         super(PiHut, self).__init__(vendor_id=PH_VENDOR_ID,
-                                         product_id=PH_PRODUCT_ID,
-                                         controls=[
-                                             Button("Select", 314, sname='select'),
-                                             Button("Left Stick", 317, sname='ls'),
-                                             Button("Right Stick", 318, sname='rs'),
-                                             Button("Start", 315, sname='start'),
-                                             Button("L1", 310, sname='l1'),
-                                             Button("L2", 312, sname='l2'),
-                                             Button("R1", 311, sname='r1'),
-                                             Button("R2", 313, sname='r2'),
-                                             Button("Triangle", 308, sname='triangle'),
-                                             Button("Circle", 305, sname='circle'),
-                                             Button("Cross", 304, sname='cross'),
-                                             Button("Square", 307, sname='square'),
-                                             Button("Analog", 316, sname='home'),
-                                             CentredAxis("Left Vertical", 0, 255, 1, invert=True, sname='ly'),
-                                             CentredAxis("Right Vertical", 0, 255, 5, invert=True, sname='ry'),
-                                             CentredAxis("Left Horizontal", 0, 255, 0, sname='lx'),
-                                             CentredAxis("Right Horizontal", 0, 255, 2, sname='rx'),
-                                             TriggerAxis("Left Trigger", 0, 255, 9, sname='lt'),
-                                             TriggerAxis("Right Trigger", 0, 255, 10, sname='rt'),
-                                             BinaryAxis("D-pad Horizontal", 16, b1name='dleft', b2name='dright'),
-                                             BinaryAxis("D-pad Vertical", 17, b1name='dup', b2name='ddown')
-                                         ],
-                                         dead_zone=dead_zone,
-                                         hot_zone=hot_zone)
+                                    product_id=PH_PRODUCT_ID,
+                                    controls=[
+                                        Button("Select", 314, sname='select'),
+                                        Button("Left Stick", 317, sname='ls'),
+                                        Button("Right Stick", 318, sname='rs'),
+                                        Button("Start", 315, sname='start'),
+                                        Button("L1", 310, sname='l1'),
+                                        Button("L2", 312, sname='l2'),
+                                        Button("R1", 311, sname='r1'),
+                                        Button("R2", 313, sname='r2'),
+                                        Button("Triangle", 308, sname='triangle'),
+                                        Button("Circle", 305, sname='circle'),
+                                        Button("Cross", 304, sname='cross'),
+                                        Button("Square", 307, sname='square'),
+                                        Button("Analog", 316, sname='home'),
+                                        CentredAxis("Left Vertical", 0, 255, 1, invert=True, sname='ly'),
+                                        CentredAxis("Right Vertical", 0, 255, 5, invert=True, sname='ry'),
+                                        CentredAxis("Left Horizontal", 0, 255, 0, sname='lx'),
+                                        CentredAxis("Right Horizontal", 0, 255, 2, sname='rx'),
+                                        TriggerAxis("Left Trigger", 0, 255, 9, sname='lt'),
+                                        TriggerAxis("Right Trigger", 0, 255, 10, sname='rt'),
+                                        BinaryAxis("D-pad Horizontal", 16, b1name='dleft', b2name='dright'),
+                                        BinaryAxis("D-pad Vertical", 17, b1name='dup', b2name='ddown')
+                                    ],
+                                    dead_zone=dead_zone,
+                                    hot_zone=hot_zone)
 
     def __repr__(self):
         return 'PiHut PS3-alike controller'

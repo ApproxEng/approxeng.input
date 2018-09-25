ce04c19eff08c564ee473b276e69e93cdef68141
Keith Ellis <keith.ellis@mac.com>
2018-09-25 15:01:26 +0000

    Switch controller class created for the left and right controllers, used seperately, not paired as one controller

--- src/python/approxeng/input/switch_L.py
+++ src/python/approxeng/input/switch_L.py
@@ -1,5 +1,5 @@
 # from approxeng.input import CentredAxis, TriggerAxis, Button, Controller, BinaryAxis
-from approxeng.input import Controller
+from approxeng.input import Controller, Button, CentredAxis
 
 SWITCH_VENDOR_ID = 1406
 SWITCH_L_PRODUCT_ID = 8198
@@ -7,12 +7,15 @@ SWITCH_L_PRODUCT_ID = 8198
 
 class SwitchJoyCon_L(Controller):
     """
-    Nintendo Switch Joycon controller, curently only the Left Controller.
+    Nintendo Switch Joycon controller, curently only the Left Controller
+	being used in horizontal mote, i.e. a single controller and not paired
+	with the righthand controller.
     """
 
     def __init__(self, dead_zone=0.05, hot_zone=0.05):
         """
         Create a new Nintendo Switch Joycon controller instance
+		Left hand contorller only
 
         :param float dead_zone:
             Used to set the dead zone for each :class:`approxeng.input.CentredAxis` and
@@ -23,7 +26,20 @@ class SwitchJoyCon_L(Controller):
         """
         super(SwitchJoyCon_L, self).__init__(vendor_id=SWITCH_VENDOR_ID,
                                            product_id=SWITCH_L_PRODUCT_ID,
-                                           controls=[],
+                                           controls=[
+                                               Button("Right", 305, sname="circle"),
+                                               Button("Up", 307, sname="triangle"),
+                                               Button("Left", 306, sname="square"),
+                                               Button("Down", 304, sname="cross"),
+                                               Button("Left Stick", 314, sname="ls"),
+                                               Button("Home", 317, sname="home"),
+                                               Button("Minus", 312, sname="start"),
+                                               Button("SL", 308, sname="l1"),
+                                               Button("SR", 309, sname="r1"),
+                                               CentredAxis("Left Horizontal", -1, 1, 16, sname="lx"),
+                                               CentredAxis("Left Vertical", 1, -1, 17, sname="ly")
+
+                                           ],
                                            dead_zone=dead_zone,
                                            hot_zone=hot_zone)
 
--- src/python/approxeng/input/switch_R.py
+++ src/python/approxeng/input/switch_R.py
@@ -1,5 +1,5 @@
 # from approxeng.input import CentredAxis, TriggerAxis, Button, Controller, BinaryAxis
-from approxeng.input import Controller
+from approxeng.input import Controller, Button, CentredAxis
 
 SWITCH_VENDOR_ID = 1406
 SWITCH_R_PRODUCT_ID = 8199
@@ -7,7 +7,9 @@ SWITCH_R_PRODUCT_ID = 8199
 
 class SwitchJoyCon_R(Controller):
     """
-    Nintendo Switch Joycon controller, curently only the Right controller.
+    Nintendo Switch Joycon controller, curently only the Right controller
+	being used in horizontal mode, i.e. a single controller and not paired
+	with the lefthand controller.
     """
 
     def __init__(self, dead_zone=0.05, hot_zone=0.05):
@@ -23,7 +25,19 @@ class SwitchJoyCon_R(Controller):
         """
         super(SwitchJoyCon_R, self).__init__(vendor_id=SWITCH_VENDOR_ID,
                                            product_id=SWITCH_R_PRODUCT_ID,
-                                           controls=[],
+                                           controls=[
+                                               Button("X", 305, sname="circle"),
+                                               Button("Y", 307, sname="triangle"),
+                                               Button("B", 306, sname="square"),
+                                               Button("A", 304, sname="cross"),
+                                               Button("Left Stick", 315, sname="ls"),
+                                               Button("Home", 316, sname="home"),
+                                               Button("Plus", 313, sname="start"),
+                                               Button("SL", 308, sname="l1"),
+                                               Button("SR", 309, sname="r1"),
+                                               CentredAxis("Left Horizontal", -1, 1, 16, sname="lx"),
+                                               CentredAxis("Left Vertical", 1, -1, 17, sname="ly")
+                                           ],
                                            dead_zone=dead_zone,
                                            hot_zone=hot_zone)
 
@@ -31,10 +45,3 @@ class SwitchJoyCon_R(Controller):
         return 'Nintendo Switch JoyCon controller'
 
 
-"""    @property
-    def battery_level(self):
-        return float(read_power_level(self.device_unique_name)) / 100.0
-
-    def __repr__(self):
-        return 'Wireless Microsoft XBox One S controller'
-"""
